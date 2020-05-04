aa886e62eb0c8e31f75c97873ea24f3da86228af
devdattakhoche <dkhoche70@gmail.com>
2020-05-04 18:39:31 +0530

    Before Format

--- components/Documentation.js
+++ components/Documentation.js
@@ -45,14 +45,15 @@ import { ScrollView } from 'react-native-gesture-handler';
          
          <Card  style={{
                     // borderColor: "black",
-                    borderRadius: 10,
-                    borderLeftColor:'blue',
-                    borderLeftWidth:2,
-                    borderRightColor:'blue',
-                    borderRightWidth:2,
-                    margin: 30
+                    borderRadius: 40,
+                    borderColor:'black',
+                    backgroundColor:"#F5F5F5",
+                    margin: 30,
+                    borderWidth : 3
                   }}>
     
+    <Card.Content>
+    <Card style =  {{borderBottomColor:'black',borderBottomWidth:2,borderRadius:10,margin:4}}>
     <Card.Content>
       <Title>Hi there!!</Title>
       <Subheading>
@@ -61,14 +62,23 @@ import { ScrollView } from 'react-native-gesture-handler';
 
           
       </Subheading>
+      </Card.Content>
+      </Card>
+      <Card style =  {{borderBottomColor:'black',borderBottomWidth:2,borderRadius:10,margin:4}}>
+    <Card.Content>
       <Title>
           Introduction:
       </Title>
-      <Subheading>
+      <Subheading> 
           This app is about the location stats for a particular location or for the current device location.
           You can also say this as Weather App , for now I haven't really thought of a name . XD
       </Subheading>
-      <Title>
+      </Card.Content>
+      </Card>
+     
+     <Card style =  {{borderBottomColor:'black',borderBottomWidth:2,borderRadius:10,margin:4}}>
+    <Card.Content> 
+    <Title>
                 Permissions required : 
       </Title>
       <Subheading>
@@ -83,6 +93,10 @@ import { ScrollView } from 'react-native-gesture-handler';
       2) Internet
                 
       </Subheading>
+      </Card.Content>
+      </Card>
+      <Card style =  {{borderBottomColor:'black',borderBottomWidth:2,borderRadius:10,margin:4}}>
+    <Card.Content>
       <Title>
           Walkthrough
       </Title>
@@ -93,6 +107,10 @@ import { ScrollView } from 'react-native-gesture-handler';
           After that you can go over to the Search City tab and search the stats for the location you want , it'll redirect you to the Weather tab , now you have to press the 'Selected City' and ka-boom  you ready!
 
       </Subheading>
+      </Card.Content>
+      </Card>
+      <Card style =  {{borderBottomColor:'black',borderBottomWidth:2,borderRadius:10,margin:4}}>
+    <Card.Content>
       <Title>
           Contact :
       </Title>
@@ -102,6 +120,8 @@ import { ScrollView } from 'react-native-gesture-handler';
           You can drop a mail here :-
           dkhoche70@gmail.com
       </Subheading>
+      </Card.Content>
+      </Card>
 
     </Card.Content>
     <Card.Cover source={{ uri: 'https://picsum.photos/700' }} style = {{margin:20}} />
